8a3fea228839606b2074229545a9b0ec7fe9ecc8
ASB2 <aboysboy@gmail.com>
2013-08-06 15:31:09 -0400

    Added some recipies

--- source/MU/CraftRegistry.java
+++ source/MU/CraftRegistry.java
@@ -1,9 +1,14 @@
 package MU;
 
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.registry.GameRegistry;
+
 public class CraftRegistry {
 
     public static void init() {
-        
+
+        GameRegistry.addShapelessRecipe(new ItemStack(BlockRegistry.BlockTopazBrick), new ItemStack(ItemRegistry.ItemTopaz));
+        GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistry.ItemTopaz), new ItemStack(BlockRegistry.BlockTopazBrick));
     }
 
 }
